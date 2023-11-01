b65ac9a6f1a41b78f3f52ce7ea52beecf2f1995c
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-01 11:21:39 -0500

    INSERT INTO

--- index.js
+++ index.js
@@ -13,8 +13,20 @@ const express = require("express");
 const app = express();
 const port ='3000'
 app.set('PORT', port)
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world from express.</h1>');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('<h1>Hello world from nuevo endpoint.</h1>');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    status: 200,
+    body: 'Ok'
+  });
 });
 
 app.listen(port, () => {
